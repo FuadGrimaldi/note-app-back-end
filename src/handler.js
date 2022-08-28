d59e7d265c3079c8ad21d76bdd62a24575fa3d44
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2022-08-28 14:37:04 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,126 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+// Menyimpan note/catatan
+const addNoteHandler = (request, h) => {
+    const { title, tags, body } = request.payload;
+
+    const id = nanoid(16);
+    const creatAt = new Date().toISOString();
+    const updateAt = creatAt;
+
+    const newNote = {
+        title, tags, body, id, creatAt, updateAt,
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'Success!',
+            message: 'Catatn berhasil ditambahkan',
+            data: {
+                noteId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'Fail!',
+        message: 'Catatan gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+};
+// Menampilkan note/catatan
+const getAllNotesHandler = () => ({
+    status: 'Success!',
+    data: {
+        notes,
+    },
+});
+/* Menampilkan note/catatn secara spesifik berdasarkan 
+ id yang digunakan oleh path parameter
+ */
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const note = notes.filter((n) => n.id === id)[0];
+
+    if (note !== undefined) {
+        return {
+            status: 'Success!',
+            data: {
+                note,
+            },
+        };
+    }
+    const response = h.response({
+        status: 'Fail!',
+        message: 'Catatn tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+// Mengedit atau mengubah note/catatan.
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const { title, tags, body } = request.payload;
+    const updateAt = new Date().toISOString();
+    
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updateAt,
+        };
+        const response = h.response({
+            status: 'Success!',
+            message: 'Catatan berhasil diperbarui',
+        });
+        response.code(200);
+        return response;
+    }
+    const response = h.response({
+        status: 'Fail!',
+        message: 'Gagal memperbarui catata. id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+// Menghapus note/catatan.
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+    
+    const index = notes.findIndex((note) => note.id === id);
+    if (index !== -1) {
+        notes.splice(index, 1);
+        const response = h.response({
+            status: 'Success!',
+            message: 'Catatan berhasil dihapus',
+        });
+        response.code(200);
+        return response;
+    }
+    const response = h.response({
+        status: 'Fail!',
+        message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = { 
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler, 
+};
